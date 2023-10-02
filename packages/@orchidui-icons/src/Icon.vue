cb0c6c883c263c80b4954dff90cc1227a2b6775e
kewcoder <kewcoder@gmail.com>
2023-10-02 23:55:14 +0700

    setup @orchidui-icons

---
+++ packages/@orchidui-icons/src/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+    <iconComp :width="width" :height="height" />
+  </template>
+  <script setup>
+  import { defineAsyncComponent } from 'vue';
+  const props = defineProps({
+    name: {
+      type: String,
+      required: true,
+    },
+    width: {
+      default: 22,
+    },
+    height: {
+      default: 22,
+    },
+  });
+  // feel free to update this with an svg directory of your choice
+  const iconComp = defineAsyncComponent(() =>
+    import(`../public/icons/${props.name}.svg?component`)
+  );
+  </script>
---
+++ packages/@orchidui-icons/src/index.js
@@ -0,0 +1,4 @@
+import Icon from './Icon.vue'
+export default {
+    Icon
+}
