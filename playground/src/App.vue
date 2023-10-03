2e6f45b378fde144c65459de5ccb267f2b40fc71
kewcoder <kewcoder@gmail.com>
2023-10-03 13:02:19 +0700

    update playground button name

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -2,12 +2,12 @@
 <template>
   <div class="w-screen h-screen flex items-center content-center justify-center flex-wrap gap-3">
     <div class="w-full p-2 text-center">Orchid UI Playground</div>
-    <Btn label="Button" />
+    <Button label="Button" />
     <Chip label="Chip" />
     <Dropdown />
   </div>
 </template>
 
 <script setup>
-import { Btn, Chip, Dropdown } from '@orchidui/vue'
+import { Button, Chip, Dropdown } from '@orchidui/vue'
 </script>
