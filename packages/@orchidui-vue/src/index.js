077d207544025ddd263916542214737cbf6eb89e
mykhailo <m.diakovych13@gmail.com>
2023-10-04 12:19:30 +0300

    Removed default storybook images, removed react from packages Added new component Accordion with slide animation and slots for header/body or option to using props to change only text

--- packages/@orchidui-vue/src/index.js
+++ packages/@orchidui-vue/src/index.js
@@ -2,3 +2,4 @@ export * from "./Button/OCButton.js";
 export * from "./Chip/OCChip.js";
 export * from "./Dropdown/OCDropdown.js";
 export * from "./Icon/OCIcon.js";
+export * from "./Accordion/OCAccordion";
