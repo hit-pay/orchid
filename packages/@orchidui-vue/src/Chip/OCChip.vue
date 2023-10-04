753dc99edf44753c057166aff6f132404372df17
kewcoder <kewcoder@gmail.com>
2023-10-04 14:21:39 +0700

    convert to composition api

--- packages/@orchidui-vue/src/Chip/OCChip.vue
+++ packages/@orchidui-vue/src/Chip/OCChip.vue
@@ -4,45 +4,42 @@
     <slot v-else />
   </span>
 </template>
-<script>
-export default {
-  props: {
-    color: {
-      type: String,
-      default: "blue",
-    },
-    label: {
-      type: String,
-    },
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: 'blue'
   },
-  computed: {
-    className() {
-      let className = "";
-      switch (this.color) {
-        case "purple":
-          className = "bg-oc-purple-50 text-oc-purple ";
-          break;
-        case "orange":
-          className = "bg-oc-warning-50 text-oc-warning ";
-          break;
-        case "red":
-          className = "bg-oc-red-50 text-oc-red ";
-          break;
-        case "dark-blue":
-          className = "bg-oc-primary-50 text-oc-primary ";
-          break;
-        case "grey":
-          className = "bg-oc-grey-100 text-oc-grey-700 ";
-          break;
-        case "green":
-          className = "bg-oc-success-50 text-oc-success ";
-          break;
-        default:
-          className = "bg-oc-blue-50 text-oc-blue ";
-          break;
-      }
-      return className;
-    },
-  },
-};
+  label: String
+});
+
+const className = computed(() => {
+  let className = "";
+  switch (props.color) {
+    case "purple":
+      className = "bg-oc-purple-50 text-oc-purple ";
+      break;
+    case "orange":
+      className = "bg-oc-warning-50 text-oc-warning ";
+      break;
+    case "red":
+      className = "bg-oc-red-50 text-oc-red ";
+      break;
+    case "dark-blue":
+      className = "bg-oc-primary-50 text-oc-primary ";
+      break;
+    case "grey":
+      className = "bg-oc-grey-100 text-oc-grey-700 ";
+      break;
+    case "green":
+      className = "bg-oc-success-50 text-oc-success ";
+      break;
+    default:
+      className = "bg-oc-blue-50 text-oc-blue ";
+      break;
+  }
+  return className;
+});
 </script>
