5e57108e8cb6ab7479b0a156b88f4c97723f698a
kewcoder <kewcoder@gmail.com>
2023-10-07 15:50:50 +0700

    add tailwind class name

--- encode_class_name.js
+++ encode_class_name.js
@@ -1,22 +1,22 @@
-
 // TODO : Ari
-
 // ENCODE TAILWIND CLASS NAME to SORT NAME
-// import replace from 'replace-in-file'
+// From tailwind_class_name.js if class name more than 7 characters
+// EXAMPLE from our class bg-oc-primary-100 to c-abcdef1
 
-// const encode_js = replace.sync({
-//     files: 'dist/assets/*.js',
-//     from: /Orchid UI/g,
-//     to: 'Orchid UI replacment',
-//     countMatches: true,
-// });
+import replace from 'replace-in-file'
 
-// const encode_css = replace.sync({
-//     files: 'dist/assets/*.css',
-//     from: /Orchid/g,
-//     to: 'Orchid',
-//     countMatches: true,
-// });
+const encode_js = replace.sync({
+    files: 'dist/assets/*.js',
+    from: /oc-btn-primary/g,
+    to: 'a-b-c',
+    countMatches: true,
+});
 
+const encode_css = replace.sync({
+    files: 'dist/assets/*.css',
+    from: /oc-btn-primary/g,
+    to: 'a-b-c',
+    countMatches: true,
+});
 
-// console.log('console :', encode_css, encode_js);
+console.log('console :', encode_css, encode_js);
