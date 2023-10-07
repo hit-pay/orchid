efe885f857c431f22504fa1e13f81fe3465785f6
kewcoder <kewcoder@gmail.com>
2023-10-07 16:21:13 +0700

    replace to random char up to 8000 class for tailwind

--- encode_class_name.js
+++ encode_class_name.js
@@ -5,18 +5,28 @@
 
 import replace from 'replace-in-file'
 
+import tailwindClassName from './tailwind_class_name.js'
+
+const ourClassName = [
+    'oc-primary-50-tr'
+] 
+// we can replace class name and variable name here
+
 const encode_js = replace.sync({
     files: 'dist/assets/*.js',
-    from: /oc-btn-primary/g,
+    from: /oc-primary/g,
     to: 'a-b-c',
     countMatches: true,
 });
 
 const encode_css = replace.sync({
     files: 'dist/assets/*.css',
-    from: /oc-btn-primary/g,
+    from: /oc-primary/g,
     to: 'a-b-c',
     countMatches: true,
 });
 
+console.log('tailwindClassName :', tailwindClassName.length, ourClassName)
 console.log('console :', encode_css, encode_js);
+// tailwind class length 7606, random up to 7606 uniq char : example o-1234 
+// ourClassName length ? , random up to ? uniq char : example c-[?]
