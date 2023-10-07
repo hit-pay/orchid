d862f33fbbe2a4a0237cf4b6e9be0c273d8e55b6
kewcoder <kewcoder@gmail.com>
2023-10-07 15:35:09 +0700

    build-file-obfuscate-class-name

---
+++ encode_class_name.js
@@ -0,0 +1,22 @@
+
+// TODO : Ari
+
+// ENCODE TAILWIND CLASS NAME to SORT NAME
+// import replace from 'replace-in-file'
+
+// const encode_js = replace.sync({
+//     files: 'dist/assets/*.js',
+//     from: /Orchid UI/g,
+//     to: 'Orchid UI replacment',
+//     countMatches: true,
+// });
+
+// const encode_css = replace.sync({
+//     files: 'dist/assets/*.css',
+//     from: /Orchid/g,
+//     to: 'Orchid',
+//     countMatches: true,
+// });
+
+
+// console.log('console :', encode_css, encode_js);
