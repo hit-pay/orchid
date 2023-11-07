b83d59564288c51d057f1cd4725f76421afbefe4
Fares Gdoura <gdourafares@gmail.com>
2023-11-07 16:21:16 +0100

    Add close button props

--- packages/@orchidui-vue/src/Feedback/Snackbar/OcSnackbar.vue
+++ packages/@orchidui-vue/src/Feedback/Snackbar/OcSnackbar.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { Icon } from "@/orchidui";
 defineProps({
+  modelValue: {
+    type: Boolean,
+    default: true,
+  },
   color: {
     type: String,
     default: "default",
@@ -17,7 +21,12 @@ defineProps({
     type: Boolean,
     default: true,
   },
+  isCloseIcon: {
+    type: Boolean,
+    default: false,
+  },
 });
+defineEmits(["update:modelValue"]);
 const colorClasses = Object.freeze({
   default: "border-oc-success-200 bg-oc-success-50 text-oc-success-500",
   primary: "border-oc-primary-200 bg-oc-primary-50 text-oc-primary-500",
@@ -29,14 +38,24 @@ const colorClasses = Object.freeze({
 
 <template>
   <div
+    v-show="modelValue"
     :class="colorClasses[color]"
     class="border rounded-lg p-4 flex items-center gap-x-3 shadow-sm"
   >
     <Icon v-if="showIcon" :name="icon" class="shrink-0" />
     <slot>
-      <span class="text-oc-text text-sm">
+      <div class="w-full flex items-center justify-between">
+        <span class="text-oc-text text-sm">
         {{ content }}
-      </span>
+        </span>
+        <div
+            v-if="isCloseIcon"
+            class="rounded cursor-pointer text-oc-gray-500 hover:bg-transparent hover:text-oc-text"
+            @click="$emit('update:modelValue', false)"
+        >
+          <Icon name="x" width="20" height="20" />
+        </div>
+      </div>
     </slot>
   </div>
 </template>
