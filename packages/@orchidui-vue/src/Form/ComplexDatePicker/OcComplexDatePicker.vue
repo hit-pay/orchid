03f9d3fcb876ed6f8090c3472f47a538971a84f2
kewcoder <kewcoder@gmail.com>
2023-11-14 13:07:02 +0700

    fix build

--- packages/@orchidui-vue/src/Form/ComplexDatePicker/OcComplexDatePicker.vue
+++ packages/@orchidui-vue/src/Form/ComplexDatePicker/OcComplexDatePicker.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { Dropdown, Input } from "@/orchidui";
-import { ComplexCalendar } from "../../ComplexCalendar";
+import ComplexCalendar from "../ComplexCalendar/OcComplexCalendar.vue";
 import { ref } from "vue";
 import dayjs from "dayjs";
 
