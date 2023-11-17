b46dbe62292a53f702b1dd9087b98bdcf0c44fc2
mykhailo <m.diakovych13@gmail.com>
2023-11-17 15:19:49 +0200

    Added component account setuo for header

--- packages/@orchidui-vue/src/Elements/Sidebar/OcSidebar.js
+++ packages/@orchidui-vue/src/Elements/Sidebar/OcSidebar.js
@@ -1,4 +1,5 @@
 import Sidebar from "./OcSidebar.vue";
 import SidebarSubmenu from "./OcSidebarSubmenu.vue";
+import AccountSetup from "./OcAccountSetup.vue";
 
-export { Sidebar, SidebarSubmenu };
+export { Sidebar, SidebarSubmenu, AccountSetup };
