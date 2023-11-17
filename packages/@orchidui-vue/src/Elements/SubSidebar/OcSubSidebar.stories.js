a31fb08422291ef049ff85321d9299bd7827991f
mykhailo <m.diakovych13@gmail.com>
2023-11-17 17:10:44 +0200

    Added component sub-sidebar

---
+++ packages/@orchidui-vue/src/Elements/SubSidebar/OcSubSidebar.stories.js
@@ -0,0 +1,54 @@
+import { Theme, SubSidebar } from "@/orchidui";
+
+export default {
+  component: SubSidebar,
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    menu: [
+      {
+        label: "Sales summary",
+        value: "sales-summary",
+      },
+      {
+        label: "Sales trends",
+        value: "sales-trends",
+      },
+      {
+        label: "General settings",
+        value: "general-settings",
+      },
+      {
+        label: "Analytics",
+        value: "analytics",
+      },
+      {
+        label: "SEO",
+        value: "seo",
+      },
+      {
+        label: "Multi-currency converter",
+        value: "multi-currency-converter",
+      },
+      {
+        label: "Order form customisation",
+        value: "order-form-customisation",
+      },
+    ],
+    title: "Title",
+    onTitleClick: () => null,
+  },
+  render: (args) => ({
+    components: { SubSidebar, Theme },
+    setup() {
+      return { args };
+    },
+    template: `
+          <Theme>
+            <SubSidebar class="w-fit" :menu="args.menu" :title="args.title" @titleClick="args.onTitleClick"/>
+          </Theme>
+        `,
+  }),
+};
