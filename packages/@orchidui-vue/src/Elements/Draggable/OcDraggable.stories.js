abdc1c1ce1bc9b4b52fdb32922c53f6b311eca11
kewcoder <kewcoder@gmail.com>
2023-11-28 12:39:21 +0700

    update code to vue 3 composition api

--- packages/@orchidui-vue/src/Elements/Draggable/OcDraggable.stories.js
+++ packages/@orchidui-vue/src/Elements/Draggable/OcDraggable.stories.js
@@ -1,37 +1,52 @@
 import { Theme } from "@/orchidui";
 
 import Draggable from "./OcDraggable.vue";
-
+import { ref } from "vue";
 export default {
   component: Draggable,
   tags: ["autodocs"],
 };
 
 export const OcDraggable = {
-  args: {
-    list: [
-      {
-        id: "123",
-        label: "Label 123",
-      },
-      {
-        id: "234",
-        label: "Label 234",
-      },
-    ],
-  },
   render: (args) => ({
     components: {
       Theme,
       Draggable,
     },
     setup() {
-      return { args };
+      const model = ref([
+        {
+          id: "123",
+          label: "123 Label ",
+        },
+        {
+          id: "234",
+          label: "234 Label ",
+        },
+        {
+          id: "456",
+          label: "456 Label ",
+        },
+        {
+          id: "272",
+          label: "272 Label ",
+        },
+        {
+          id: "667",
+          label: "667 Label ",
+        },
+        {
+          id: "781",
+          label: "781 Label ",
+        },
+      ]);
+      return { args, model };
     },
     template: `
           <Theme>
             <div class="w-full h-[200px]">
-              <Draggable :list="args.list" ></Draggable>
+                {{ model}}
+              <Draggable v-model="model"></Draggable>
             </div>
           </Theme>
         `,
--- packages/@orchidui-vue/src/Elements/Draggable/OcDraggable.vue
+++ packages/@orchidui-vue/src/Elements/Draggable/OcDraggable.vue
@@ -1,44 +1,45 @@
+<script setup>
+import { VueDraggableNext } from "vue-draggable-next";
+import { Icon } from "@/orchidui";
+import { ref } from "vue";
+
+const props = defineProps("modelValue");
+const emit = defineEmits("update:modelValue");
+
+const list = ref(props.modelValue);
+
+const onChange = () => {
+  emit("update:modelValue", this.list);
+};
+</script>
 <template>
-  <VueDraggableNext class="grid gap-3" :list="list" handle=".drag-el" @change="updated">
+  <VueDraggableNext
+    class="grid gap-3"
+    handle=".drag-el"
+    :list="list"
+    @change="onChange"
+  >
     <transition-group name="list">
       <slot v-if="$slots.list" name="list" :list="list" />
       <template v-else>
-        <div 
-          v-for="element in list" :key="element.id"
-           class="bg-oc-accent-1-50 hover:bg-oc-gray-50 text-oc-text-500 p-4 flex rounded" >
-            <span class="cursor-move drag-el">
-              <Icon name="draggable" />
-            </span>
-            <span class="ml-2">{{ element.label }}</span>
-            <span>
-              <Icon name="dot-vertical" />
-            </span>
+        <div
+          v-for="element in list"
+          :key="element.id"
+          class="bg-oc-accent-1-50 hover:bg-oc-gray-50 text-oc-text-500 p-4 flex rounded"
+        >
+          <span class="cursor-move drag-el">
+            <Icon name="draggable" />
+          </span>
+          <span class="ml-2">{{ element.label }}</span>
+          <span>
+            <Icon name="dot-vertical" />
+          </span>
         </div>
       </template>
     </transition-group>
   </VueDraggableNext>
 </template>
 
-<script setup>
-import { Icon } from "@/orchidui";
-import { VueDraggableNext } from "vue-draggable-next";
-</script>
-<script>
-export default {
-  props: ["list", "modelValue"],
-  data() {
-    return {
-      enabled: true,
-      dragging: false,
-    };
-  },
-  methods: {
-    updated() {
-      this.$emit("update:modelValue", this.list);
-    },
-  },
-};
-</script>
 <style>
 .grabbing * {
   cursor: grabbing;
