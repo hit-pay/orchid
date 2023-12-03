de4f7c74ae3db1fc0a3552a3317fa3d103aa4e97
kewcoder <kewcoder@gmail.com>
2023-12-04 04:51:10 +0700

    clean up

--- packages/@orchidui-vue/src/StoreDesign/RequestForm/RequestForm.vue
+++ packages/@orchidui-vue/src/StoreDesign/RequestForm/RequestForm.vue
@@ -1,12 +1,3 @@
-<!-- Grid selection -> preview image [pro feature] -> horizontal image, horizontal text desc, vertical -->
-<!-- Section Item, Input, TextAre, Select, Select Product -->
-<!-- Custom Slot -->
-
-<!-- Feature : Hidden/Show base on Condition Other Input -->
-<!-- Feature : Hidden/Show base on Free on Pro Settings -->
-
-<!-- Used in Home & Section Settings-->
-<!-- Working on this -->
 
 <script setup>
 import { Input, Select, TextArea } from "@/orchidui";
