39e47cad07633c256349205148595995a27ef707
kewcoder <kewcoder@gmail.com>
2023-12-04 01:36:17 +0700

    request form options grid and list

--- packages/@orchidui-vue/src/StoreDesign/RequestForm/RequestForm.stories.js
+++ packages/@orchidui-vue/src/StoreDesign/RequestForm/RequestForm.stories.js
@@ -1,20 +1,177 @@
 import { Theme } from "@/orchidui";
 import { RequestForm } from "@/orchidui/StoreDesign";
+
+import { ref } from "vue";
 export default {
   tags: ["autodocs"],
   component: RequestForm,
 };
 
 export const Default = {
-  args: {},
+  args: {
+    jsonForm: [
+      {
+        name: "select_options",
+        type: "SelectOptions",
+        props: {
+          label: "Example Label",
+          hint: "This is a hint text to help user",
+          placeholder: "placeholder",
+          options: [
+            {
+              preview: "/images/request-form.svg",
+              label: "Option 1",
+              value: 1,
+            },
+            {
+              preview: "/images/request-form.svg",
+              label: "Option 2",
+              value: 2,
+            },
+            {
+              preview: "/images/request-form.svg",
+              label: "Option 3",
+              value: 3,
+            },
+          ],
+        },
+      },
+      {
+        name: "select_options_2",
+        type: "SelectOptions",
+        props: {
+          label: "Example Label",
+          options: [
+            {
+              preview: "/images/request-form.svg",
+              label: "Option 1",
+              value: 1,
+            },
+            {
+              preview: "/images/request-form.svg",
+              label: "Option 2",
+              value: 2,
+            },
+          ],
+        },
+      },
+      {
+        name: "select_options_3",
+        type: "SelectOptions",
+        props: {
+          variant: "list",
+          label: "Example Label",
+          hint: "This is a hint text to help user",
+          placeholder: "placeholder",
+          options: [
+            {
+              preview: "/images/request-form-1.svg",
+              label: "Option 1",
+              value: 1,
+              description:
+                "Useful if most of your product visuals are wider than taller",
+            },
+            {
+              preview: "/images/request-form-1.svg",
+              label: "Option 2",
+              value: 2,
+              description:
+                "Useful if most of your product visuals are wider than taller",
+            },
+            {
+              preview: "/images/request-form-1.svg",
+              label: "Option 3",
+              value: 3,
+              description:
+                "Useful if most of your product visuals are wider than taller",
+            },
+            {
+              preview: "/images/request-form-1.svg",
+              label: "Option 4",
+              value: 4,
+              description:
+                "Useful if most of your product visuals are wider than taller",
+            },
+          ],
+        },
+      },
+      {
+        name: "input",
+        type: "Input",
+        props: {
+          label: "Example Label",
+          hint: "This is a hint text to help user",
+          placeholder: "placeholder",
+        },
+      },
+      {
+        name: "select",
+        type: "Select",
+        props: {
+          label: "Example Label",
+          hint: "This is a hint text to help user",
+          placeholder: "placeholder",
+          options: [
+            {
+              label: "Option 1",
+              value: 1,
+            },
+            {
+              label: "Option 2",
+              value: 2,
+            },
+            {
+              label: "Option 3",
+              value: 3,
+            },
+          ],
+        },
+      },
+      {
+        name: "textarea",
+        key: "textarea",
+        type: "Textarea",
+        props: {
+          label: "Example Label",
+          hint: "This is a hint text to help user",
+          placeholder: "placeholder",
+        },
+      },
+    ],
+  },
   render: (args) => ({
     components: { Theme, RequestForm },
     setup() {
-      return { args };
+      const values = ref({
+        select_options: "",
+        select_options_1: "",
+        select_options_2: "",
+        input: "",
+        select: "",
+        textarea: "",
+      });
+      const errors = ref({});
+
+      const onUpdateForm = (form, value = null) => {
+        if (typeof form.name === "object") {
+          form.name.forEach((formName, index) => {
+            values.value[formName.key] = value[index];
+          });
+        } else {
+          values.value[form.name] = value;
+        }
+      };
+
+      return { values, errors, onUpdateForm, args };
     },
     template: `
           <Theme>
-            <RequestForm></RequestForm>
+            <div class="mb-5">
+              {{ values}}
+            </div>
+            <div class="max-w-[550px] border p-4 rounded">
+              <RequestForm class="grid gap-5"  id="section_name" :values="values" :errors="errors" :json-form="args.jsonForm"  @onUpdate="onUpdateForm" />
+            </div>
           </Theme>
         `,
   }),
--- packages/@orchidui-vue/src/StoreDesign/RequestForm/types/SelectOptions.vue
+++ packages/@orchidui-vue/src/StoreDesign/RequestForm/types/SelectOptions.vue
@@ -1,7 +1,101 @@
+<script setup>
+import { BaseInput } from "@/orchidui";
+
+import { computed } from "vue";
+
+const props = defineProps({
+  variant: {
+    type: String,
+    default: "grid",
+  },
+  label: String,
+  hint: String,
+  errorMessage: String,
+  placeholder: String,
+  isDisabled: Boolean,
+  options: Array,
+  modelValue: [Array, String, Number],
+  isRequired: {
+    type: Boolean,
+    default: false,
+  },
+  labelIcon: {
+    type: String,
+    default: "",
+  },
+  tooltipText: {
+    type: String,
+    default: "",
+  },
+  tooltipOptions: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+
+const emit = defineEmits({
+  "update:modelValue": [],
+});
+
+const update = (value) => {
+  emit("update:modelValue", value);
+};
+
+const isVariantGrid = computed(() => {
+  return props.variant === "grid";
+});
+</script>
+
 <template>
-  <div>
-    Select Options with preview image
-    <!-- grid -->
-    <!-- col -->
-  </div>
+  <BaseInput
+    :label="isInlineLabel ? '' : label"
+    :hint="hint"
+    :error-message="errorMessage"
+    :is-required="isRequired"
+    :label-icon="labelIcon"
+    :tooltip-text="tooltipText"
+    :tooltip-options="tooltipOptions"
+  >
+    <div
+      class="w-full grid gap-5"
+      :class="variant === 'grid' ? 'grid-cols-3' : ''"
+    >
+      <div
+        v-for="opt in options"
+        :key="opt.value"
+        class="cursor-pointer"
+        :class="{
+          'flex border border-oc-gray-200 p-4 rounded hover:shadow':
+            !isVariantGrid,
+          'border-2 border-oc-primary':
+            !isVariantGrid && opt.value === modelValue,
+        }"
+        @click="update(opt.value)"
+      >
+        <img
+          class="hover:shadow rounded"
+          :class="{
+            'border-2 border-oc-primary':
+              isVariantGrid && opt.value === modelValue,
+            'border border-oc-gray-200':
+              (isVariantGrid && opt.value !== modelValue) || !isVariantGrid,
+            'w-full': isVariantGrid,
+            'w-1/8': !isVariantGrid,
+          }"
+          :src="opt.preview"
+        />
+        <div class="mt-2" :class="isVariantGrid ? 'text-center' : 'px-3'">
+          <span class="font-medium" :class="isVariantGrid ? 'text-sm ' : ''">{{
+            opt.label
+          }}</span>
+          <div
+            v-if="opt.description"
+            class="flex text-sm text-oc-text-400 mt-2"
+          >
+            {{ opt.description }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </BaseInput>
 </template>
