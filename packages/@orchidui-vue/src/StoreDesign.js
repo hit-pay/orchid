b74ca806ae9ac5ddc08b589ac6adb467d8d3b2b5
kewcoder <kewcoder@gmail.com>
2023-12-07 01:12:38 +0700

    remove request form

--- packages/@orchidui-vue/src/StoreDesign.js
+++ packages/@orchidui-vue/src/StoreDesign.js
@@ -2,6 +2,5 @@ import SDSidebar from "./StoreDesign/SDSidebar/SDSidebar.vue";
 import SDMenus from "./StoreDesign/SDMenus/SDMenus.vue";
 import ThumbnailTheme from "./StoreDesign/ThumbnailTheme/ThumbnailTheme.vue";
 import ThumbnailSection from "./StoreDesign/ThumbnailSection/ThumbnailSection.vue";
-import RequestForm from "./StoreDesign/RequestForm/RequestForm.vue";
 
-export { SDSidebar, SDMenus, ThumbnailTheme, ThumbnailSection, RequestForm };
+export { SDSidebar, SDMenus, ThumbnailTheme, ThumbnailSection };
