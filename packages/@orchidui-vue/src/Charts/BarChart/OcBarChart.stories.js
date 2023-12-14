ff3443cc47421d88c6a733b81e1b1919e785f9aa
kutsaniuk <kutsaniuk@gmail.com>
2023-12-14 18:07:04 +0100

    Add Bar Chart

---
+++ packages/@orchidui-vue/src/Charts/BarChart/OcBarChart.stories.js
@@ -0,0 +1,53 @@
+import BarChart from "./OcBarChart.vue";
+
+export default {
+  component: BarChart,
+  tags: ["autodocs"],
+};
+
+export const Line = {
+  args: {
+    color: '#D8A4F6',
+    hoverColor: '#B14AED',
+    showGrid: false,
+    showTooltip: true,
+    showLegend: true,
+    chartData: [
+      500, 730, 824, 1218.88, 2135, 1147, 760, 1150, 2130, 2324, 1218, 2135, 1147,
+    ],
+    labelData: [
+      "NOV'22",
+      "DEC'22",
+      "JAN'23",
+      "FEB'23",
+      "MAR'23",
+      "MAY'23",
+      "JUN'23",
+      "JUL'23",
+      "AUG'23",
+      "SEP'23",
+      "OCT'23",
+      "NOV'23",
+    ],
+  },
+  render: (args) => ({
+    components: { BarChart },
+    setup() {
+      return { args };
+    },
+    template: `
+          <div>
+            <BarChart
+                class="h-[300px]"
+                :color="args.color"
+                :hover-color="args.hoverColor"
+                :show-grid="args.showGrid"
+                :show-tooltip="args.showTooltip"
+                :show-legend="args.showLegend"
+                :chart-data="args.chartData"
+                :label-data="args.labelData"
+            />
+          </div>
+        `,
+  }),
+};
---
+++ packages/@orchidui-vue/src/Charts/BarChart/OcBarChart.vue
@@ -0,0 +1,126 @@
+<template>
+  <div ref="barChart" class="w-full" />
+
+</template>
+
+<script setup lang="ts">
+import * as echarts from "echarts";
+import { computed, onMounted, ref, watch } from "vue";
+import {Tooltip} from "@/orchidui";
+
+const props = defineProps({
+  color: String,
+  hoverColor: String,
+  showTooltip: Boolean,
+  showLegend: Boolean,
+  showGrid: Boolean,
+  chartData: Array,
+  labelData: Array,
+});
+const markLineData = ref({
+  index: 0,
+  value: 0,
+});
+const options = computed(() => ({
+  xAxis: {
+    type: "category",
+    data: props.labelData,
+    axisTick: {
+      show: false,
+    },
+    axisLabel: {
+      color: "#9295A5",
+    },
+    axisLine: {
+      lineStyle: {
+        color: "#F2F2F4",
+      },
+    },
+  },
+  yAxis: {
+    type: "value",
+    splitLine: {
+      show: false,
+    },
+    axisLabel: {
+      color: "#9295A5",
+      formatter: (value) => {
+        return (value / 1000).toFixed(1) + "K";
+      },
+    },
+  },
+  grid: {
+    show: props.showGrid,
+    right: 0,
+    left: 0,
+    top: "10px",
+    bottom: 0,
+    containLabel: true,
+  },
+  legend: {
+    show: props.showLegend,
+  },
+  tooltip: {
+    show: props.showTooltip,
+    padding: 0,
+    borderWidth: 0,
+    formatter: (params) => {
+      const currency = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'SGD',
+      });
+
+      return `
+        <div class="py-3 px-4 leading-normal">
+            <div class="flex w-full justify-between items-center">
+                <span class="uppercase text-[10px] font-medium">
+                    ${params.name}
+                </span>
+            </div>
+            <div class="text-oc-text font-medium text-[12px]">${currency.format(params.value)}</div>
+        </div>
+
+      `;
+    },
+  },
+  series: [
+    {
+      data: props.chartData,
+      type: "bar",
+      smooth: true,
+      showSymbol: false,
+      color: props.color,
+      emphasis: {
+        itemStyle: {
+          color: props.hoverColor,
+        }
+      },
+    },
+  ],
+}));
+const myChart = ref();
+const barChart = ref();
+
+const toggleLegendName = (name) => {
+  myChart.value.dispatchAction({
+    type: "legendToggleSelect",
+    name,
+  });
+};
+
+defineExpose({
+  toggleLegendName,
+});
+
+onMounted(() => {
+  myChart.value = echarts.init(barChart.value);
+  myChart.value.setOption(options.value);
+});
+watch(
+  () => options.value,
+  (val) => {
+    myChart.value.setOption(val);
+  },
+  { deep: true },
+);
+</script>
