0d42a145e72e924ab707fc012e2b620ec9497dc0
kutsaniuk <kutsaniuk@gmail.com>
2023-12-15 12:59:32 +0100

    Minor fix

--- packages/@orchidui-vue/src/Charts/index.js
+++ packages/@orchidui-vue/src/Charts/index.js
@@ -1,2 +1,4 @@
 export * from "./LineChart/OcLineChart.js";
 export * from "./PieChart/OcPieChart.js";
+export * from "./BarChart/OcBarChart.vue";
+export * from "./BarRaceChart/OcBarRaceChart.vue";
