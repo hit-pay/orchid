62770b5d1d2272dfe3f152f63969a0bfdc5f7f42
mykhailo <m.diakovych13@gmail.com>
2023-12-22 10:49:19 +0200

    Fixed mobile view for copy link Fixed mobile view for sub-sidebar Fixed text-overflow in input

--- packages/@orchidui-vue/src/Elements/SubSidebar/OcSubSidebar.vue
+++ packages/@orchidui-vue/src/Elements/SubSidebar/OcSubSidebar.vue
@@ -22,17 +22,22 @@ defineEmits(["titleClick", "update:modelValue"]);
   <div class="py-7 flex flex-col gap-y-3 px-4">
     <div
       v-if="title"
-      class="py-4 gap-x-3 cursor-pointer flex font-medium items-center"
+      class="py-4 gap-x-3 whitespace-nowrap cursor-pointer flex font-medium items-center"
       @click="$emit('titleClick')"
     >
-      <Icon name="chevron-down" class="rotate-90" width="20" height="20" />
+      <Icon
+        name="chevron-down"
+        class="rotate-90 shrink-0"
+        width="20"
+        height="20"
+      />
       {{ title }}
     </div>
     <div class="flex flex-col gap-y-2">
       <div
         v-for="(item, i) in menu"
         :key="i"
-        class="py-3 rounded cursor-pointer font-medium hover:bg-oc-accent-2-50-tr px-5"
+        class="py-3 whitespace-nowrap rounded cursor-pointer font-medium hover:bg-oc-accent-2-50-tr px-5"
         :class="
           modelValue === item.value
             ? 'bg-oc-accent-2-50-tr text-oc-accent-1'
