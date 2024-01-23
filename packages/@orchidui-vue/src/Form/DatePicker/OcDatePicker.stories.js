2e6fd60631d27f101a97cc23d56453824c75a9d8
Fares Gdoura <gdourafares@gmail.com>
2024-01-23 16:46:17 +0400

    set default value to null

--- packages/@orchidui-vue/src/Form/DatePicker/OcDatePicker.stories.js
+++ packages/@orchidui-vue/src/Form/DatePicker/OcDatePicker.stories.js
@@ -31,7 +31,7 @@ export const Default = {
     hint: "",
     minLabel: "From",
     maxLabel: "To",
-    minDate: null,
+    minDate: dayjs(),
     maxDate: null,
     isRequired: true,
     label: "Date",
