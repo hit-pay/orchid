e0556acffd595722ce1d1a834be9f63bf7899bdf
HoaiLee <>
2024-01-31 22:56:37 +0700

    option slot

--- packages/@orchidui-vue/src/Form/Select/OcOption.vue
+++ packages/@orchidui-vue/src/Form/Select/OcOption.vue
@@ -14,17 +14,19 @@ defineProps({
     class="p-3 flex items-center text-oc-text-400 justify-between cursor-pointer hover:bg-oc-accent-1-50 gap-x-3 rounded-sm"
     :class="isSelected && !isCheckboxes && 'bg-oc-accent-1-50'"
   >
-    <Checkbox
-      v-if="isCheckboxes"
-      :model-value="!!isSelected"
-      class="!w-fit pointer-events-none"
-    />
-    <div class="flex flex-col gap-3" :class="{ 'flex-1': isCheckboxes }">
-      <span>{{ label }}</span>
-      <span v-if="subLabel" class="text-sm text-oc-text-300">{{
-        subLabel
-      }}</span>
-    </div>
+    <slot>
+      <Checkbox
+        v-if="isCheckboxes"
+        :model-value="!!isSelected"
+        class="!w-fit pointer-events-none"
+      />
+      <div class="flex flex-col gap-3" :class="{ 'flex-1': isCheckboxes }">
+        <span>{{ label }}</span>
+        <span v-if="subLabel" class="text-sm text-oc-text-300">{{
+          subLabel
+        }}</span>
+      </div>
+    </slot>
     <Icon
       v-if="isSelected && !isCheckboxes"
       class="w-5 h-5 text-oc-primary"
