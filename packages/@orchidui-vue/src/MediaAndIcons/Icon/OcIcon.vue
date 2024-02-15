f5c126031cf0ad48ca8d0173535b0ab4ef3f6889
kewcoder <kewcoder@gmail.com>
2024-02-15 18:44:50 +0700

    format

--- packages/@orchidui-vue/src/MediaAndIcons/Icon/OcIcon.vue
+++ packages/@orchidui-vue/src/MediaAndIcons/Icon/OcIcon.vue
@@ -1,9 +1,14 @@
-<template >
-  <svg ref="iconRef" :width="`${props.width}px`" :height="`${props.height}px`" :viewBox="`0 0 ${props.width} ${props.height}`"></svg>
+<template>
+  <svg
+    ref="iconRef"
+    :width="`${props.width}px`"
+    :height="`${props.height}px`"
+    :viewBox="`0 0 ${props.width} ${props.height}`"
+  ></svg>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 const props = defineProps({
   path: {
     type: String,
@@ -20,12 +25,12 @@ const props = defineProps({
   height: {
     type: String,
     default: "24",
-  }
+  },
 });
-const iconRef = ref(null)
+const iconRef = ref(null);
 fetch(`${props.path}/${props.name}.svg`)
-  .then(r => r.text())
-  .then(text => {
-    iconRef.value.innerHTML = text
-  })
+  .then((r) => r.text())
+  .then((text) => {
+    iconRef.value.innerHTML = text;
+  });
 </script>
