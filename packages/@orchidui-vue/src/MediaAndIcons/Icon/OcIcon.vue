66d116b1de794a29b9a32d8c81ef963d88fbd39d
kewcoder <kewcoder@gmail.com>
2024-02-15 18:32:02 +0700

    fix oc-icon for some browser

--- packages/@orchidui-vue/src/MediaAndIcons/Icon/OcIcon.vue
+++ packages/@orchidui-vue/src/MediaAndIcons/Icon/OcIcon.vue
@@ -1,15 +1,10 @@
-<template>
-  <svg
-    :width="`${width}px`"
-    :height="`${height}px`"
-    :viewBox="`0 0 ${width} ${height}`"
-  >
-    <use :xlink:href="`${path}/${name}.svg#orchid`"></use>
-  </svg>
+<template >
+  <svg ref="iconRef" :width="`${props.width}px`" :height="`${props.height}px`" :viewBox="`0 0 ${props.width} ${props.height}`"></svg>
 </template>
 
 <script setup>
-defineProps({
+import { ref } from 'vue'
+const props = defineProps({
   path: {
     type: String,
     default: "/icons/orchidui",
@@ -25,6 +20,12 @@ defineProps({
   height: {
     type: String,
     default: "24",
-  },
+  }
 });
+const iconRef = ref(null)
+fetch(`${props.path}/${props.name}.svg`)
+  .then(r => r.text())
+  .then(text => {
+    iconRef.value.innerHTML = text
+  })
 </script>
