d47b3441cbee33a82b180dbf227df95dcddcd448
kewcoder <kewcoder@gmail.com>
2024-02-26 23:15:18 +0700

    setup

---
+++ packages/@orchidui-storefront/src/index.js
@@ -0,0 +1,17 @@
+// Storefront
+class Storefront {
+    
+    constructor(id) {
+        this.businessId = id
+    }
+    addToCart(data, calback){
+        const {product, variantId, quantity} = data
+        console.log(`${quantity} product ${product.name} ${variantId}, added to cart`)
+        return calback()
+    }
+    visit(page){
+        console.log(`redirect ${page}`)
+    }
+}
+
+export default Storefront
