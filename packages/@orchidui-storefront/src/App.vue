4ffc777c5ce69b3e74bdd7403c72c4f3e9b78b1f
kewcoder <kewcoder@gmail.com>
2024-02-29 15:04:04 +0700

    publish default ui

--- packages/@orchidui-storefront/src/App.vue
+++ packages/@orchidui-storefront/src/App.vue
@@ -1,45 +1,52 @@
 <template>
-    <div>
-        Testing
-        <s-product-card :state="state" />
-    </div>
+  <div>
+    Testing
+    <s-product-card :state="state" />
+  </div>
 </template>
 <script setup>
-import { reactive, ref,  defineCustomElement } from 'vue'
+import { reactive, ref, defineCustomElement } from "vue";
 // html only in orchid theme builder : will get html data from props laravel blade
-import BtnPrimary from './ui/btn-primary.js'
-import ProductCard from './ui/product-card.js'
+import { BtnPrimary, ProductCard } from "./ui.js";
 
-
-
-const state = reactive({}) // global state
+const state = reactive({}); // global state
 // s-btn-primary
-const BtnPrimaryTemplate = BtnPrimary.replaceAll('{','{{').replaceAll('}','}}')
-customElements.define('s-btn-primary', defineCustomElement({
+const BtnPrimaryTemplate = BtnPrimary.replaceAll("{", "{{").replaceAll(
+  "}",
+  "}}"
+);
+customElements.define(
+  "s-btn-primary",
+  defineCustomElement({
     props: {
-        label: String
+      label: String,
     },
     template: BtnPrimaryTemplate,
-}))
+  })
+);
 
 // s-product-card
-const ProductCardTemplate = ProductCard.replaceAll('{','{{').replaceAll('}','}}')
-customElements.define('s-product-card', defineCustomElement({
-    setup(){
-    const product = ref({
-            id: 1,
-            name: 'item 1',
-            price: 100,
-            image: 'https://via.placeholder.com/150',
-            has_variations: true,
-            variations_count: 4
-        })
-    return {
-        product
-    }
+const ProductCardTemplate = ProductCard.replaceAll("{", "{{").replaceAll(
+  "}",
+  "}}"
+);
+customElements.define(
+  "s-product-card",
+  defineCustomElement({
+    setup() {
+      const product = ref({
+        id: 1,
+        name: "item 1",
+        price: 100,
+        image: "https://via.placeholder.com/150",
+        has_variations: true,
+        variations_count: 4,
+      });
+      return {
+        product,
+      };
     },
     template: ProductCardTemplate,
-}))
-
-
+  })
+);
 </script>
---
+++ packages/@orchidui-storefront/src/ui.js
@@ -0,0 +1,7 @@
+import BtnPrimary from "./ui/btn-primary.js";
+import ProductCard from "./ui/product-card.js";
+
+export {
+    BtnPrimary,
+    ProductCard
+}
