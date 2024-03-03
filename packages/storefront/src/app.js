ccc86a443397bbf3899f98d9842f36cf7b483b19
kewcoder <kewcoder@gmail.com>
2024-03-03 22:51:16 +0700

    clean up code

---
+++ packages/storefront/src/app.js
@@ -0,0 +1,55 @@
+import { createApp , ref} from "vue";
+import App from "@/App.vue";
+import "@/scss/tailwind.scss";
+
+// Dynamic UI Templates
+// html only in orchid theme builder : will get html data from props laravel blade
+import { BtnPrimary, ProductCard } from "./ui.js";
+// s-btn-primary
+const BtnPrimaryTemplate = BtnPrimary.replaceAll("{", "{{").replaceAll(
+  "}",
+  "}}"
+);
+// s-product-card
+const ProductCardTemplate = ProductCard.replaceAll("{", "{{").replaceAll(
+  "}",
+  "}}"
+);
+
+
+
+const app = createApp(App);
+
+const SBtnPrimary = {
+    props: {
+      label: String,
+    },
+    template: BtnPrimaryTemplate,
+}
+
+app.component("SBtnPrimary", SBtnPrimary);
+
+const SProductCard =  {
+    props: {
+      state: Object,
+    },
+    setup() {
+      const product = ref({
+        id: 1,
+        name: "item 1",
+        price: 100,
+        image: "https://via.placeholder.com/150",
+        has_variations: true,
+        variations_count: 4,
+      });
+      return {
+        product,
+        addToCart: () => {
+          console.log("add to cart");
+        },
+      };
+    },
+    template: ProductCardTemplate,
+}
+app.component("SProductCard",SProductCard);
+app.mount("#app");
