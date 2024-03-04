622193ca967a9f69c8f713ad91b062f7487011b0
kewcoder <kewcoder@gmail.com>
2024-03-04 16:28:47 +0700

    fix render components

--- packages/storefront/src/app.js
+++ packages/storefront/src/app.js
@@ -1,55 +1,74 @@
 import { createApp , ref} from "vue";
 import App from "@/App.vue";
 import "@/scss/tailwind.scss";
+import { defineAsyncComponent } from 'vue'
 
-const mountEl = document.querySelector("#app");
-const props = { ...mountEl.dataset };
-const components = JSON.parse(props.components)
+
+
+
 const convertToVueTemplate = (string) => {
   return string.replaceAll("{", "{{").replaceAll(
     "}",
     "}}"
   );
 }
-const BtnPrimaryTemplate = convertToVueTemplate(components['SBtnPrimary'])
-const ProductCardTemplate = convertToVueTemplate(components['SProductCard'])
+
 const app = createApp(App);
 
-// DEMO rendering components
-
-
-// setup all props, methods. emit
-const SBtnPrimary = {
-    props: {
-      label: String,
-    },
-    template: BtnPrimaryTemplate,
-}
-const SProductCard =  {
-    props: {
-      state: Object,
-    },
-    setup() {
-      const product = ref({
-        id: 1,
-        name: "item 1",
-        price: 100,
-        image: "https://via.placeholder.com/150",
-        has_variations: true,
-        variations_count: 4,
-      });
-      return {
-        product,
-        addToCart: () => {
-          console.log("add to cart");
-        },
-      };
-    },
-    template: ProductCardTemplate,
-}
-
-
 // register components
-app.component("SProductCard",SProductCard);
-app.component("SBtnPrimary", SBtnPrimary);
+app.component("SBtnPrimary", defineAsyncComponent(() => {
+  return new Promise((resolve, reject) => {
+   fetch('/btn-primary.html')
+   .then((r) => r.text())
+   .then(template => {
+      const SBtnPrimary = {
+          props: {
+            label: String,
+          },
+          template: convertToVueTemplate(template)
+      }
+      resolve(SBtnPrimary)
+   }).catch((err) => {
+    console.log("component response", err)
+    reject('component not found')
+   })
+  })
+}));
+
+app.component("SProductCard", defineAsyncComponent(() => {
+  return new Promise((resolve, reject) => {
+   fetch('/product-card.html')
+   .then((r) => r.text())
+   .then(template => {
+    const SProductCard =  {
+      props: {
+        state: Object,
+      },
+      setup() {
+        const product = ref({
+          id: 1,
+          name: "item 1",
+          price: 100,
+          image: "https://via.placeholder.com/150",
+          has_variations: true,
+          variations_count: 4,
+        });
+        return {
+          product,
+          addToCart: () => {
+            console.log("add to cart");
+          },
+        };
+      },
+      template: convertToVueTemplate(template),
+    }
+    resolve(SProductCard)
+   }).catch((err) => {
+    console.log("component response", err)
+    reject('component not found')
+   })
+  })
+}));
+
+
 app.mount("#app");
