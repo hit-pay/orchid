d960cacde6f20b1869407ebfb788869cb57447bf
kewcoder <kewcoder@gmail.com>
2024-03-05 13:08:09 +0700

    publish storefront esm-builder

--- packages/storefront/src/App.vue
+++ packages/storefront/src/App.vue
@@ -1,14 +1,5 @@
 <template>
- <s-headers  />
+ <s-headers/>
 </template>
 <script setup>
-// import { ref } from "vue";
-// const product = ref({
-//   id: 1,
-//   name: "item 1",
-//   price: 100,
-//   image: "https://via.placeholder.com/150",
-//   has_variations: true,
-//   variations_count: 4,
-// });
 </script>
