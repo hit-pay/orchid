22fcaa5f466104fb2e522d55abe687f71bdd75c7
kewcoder <kewcoder@gmail.com>
2024-03-05 18:49:43 +0700

    update github action

--- packages/storefront/vite.config.js
+++ packages/storefront/vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
 export default defineConfig({
-  publicDir: false, // remove on development : TODO : use env 
+  // publicDir: false, // remove on development : TODO : use env 
   plugins: [vue()],
   server: {
     hmr: false
