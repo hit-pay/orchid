e0e87cf313bde3f0f1cf930abe834de860f7e6f8
kewcoder <kewcoder@gmail.com>
2024-03-05 11:13:03 +0700

    useStorefront composable package

--- packages/storefront/src/app.js
+++ packages/storefront/src/app.js
@@ -5,58 +5,21 @@ import { defineAsyncComponent, ref } from "vue";
 import { components } from "./components";
 import storefront from "./storefront.json"
 import products from "./products-home.json"
+import { useStorefront } from "./index";
 const convertToVueTemplate = (string) => {
   const result = string.replaceAll("{#", "{{").replaceAll("#}", "}}").replace('<script type="module" src="/@vite/client"></script>','');
   return result;
 };
 const path = "/components/";
-let homeState = {}
 
-storefront.store_design.sections.forEach((s) => {
-  if(s.group === 'sections'){
-    homeState[s.key] = {
-      products: [],
-      meta: {}
-    }
-  }
-})
 
-const state = ref({
-  business: storefront.business,
-  design: storefront.store_design,
-  home: homeState,
-  product: {},
-  search: {
-    meta: {
-      keyword: '',
-      categories: [],
-      page: 1,
-      per_page: 10
-    },
-    products: products
-  }
-});
+const { state, action, initialState } = useStorefront()
 
-const action = ref({
-  addToCart: () => {
-    console.log("add to cart");
-  },
-  viewPage: (path) => {
-    console.log("View page path :", path)
-  },
-  getProducts: (section, from, category, ids) => {
-    console.log(section, from, category, ids)
-    // 1. featured
-    // 2. all products
-    // 3. category
-    // 5. pick products ids
-    // api set state.home['seactionName'].products
-    // api set state.home['seactionName'].meta
-  },
-  searchProducts: () => {
-      // api set state.search.products
-  }
-});
+initialState(storefront)
+
+setTimeout(() => {
+  action.setSectionState("product_list_1", "product", products)
+}, 1000)
 
 const app = createApp(VueApp);
 components.forEach((comp, index) => {
---
+++ packages/storefront/src/index.js
@@ -0,0 +1,76 @@
+
+import { ref, computed } from 'vue'
+export function useStorefront() {
+
+    const business = ref({})
+    const store_design = ref({})
+
+    const state = ref({
+        sections: {},
+        product: {},
+        search: {
+            meta: {
+                keyword: '',
+                categories: [],
+                page: 1,
+                per_page: 10
+            },
+            products: {}
+        },
+        cart: {}
+    });
+    const sections = computed(() => store_design.value.sections)
+
+    const initialState = (storefront) => {
+        let defaultState = {}
+        storefront.store_design.sections.forEach((s) => {
+            if(s.group === 'sections'){
+                defaultState[s.key] = {
+                    products: [],
+                    meta: {}
+                }
+            }
+        })
+        state.value.sections = defaultState
+        business.value =  storefront.business
+        store_design.value = storefront.store_design
+    } 
+
+    const setState = (key, value) => {
+        state.value[key] = value
+    }
+
+    const setSectionState = (name, key, value) => {
+        state.value.sections[name][key] = value
+    }
+
+    const setProductState = (product) => {
+        state.value.product = product
+    }
+
+    const action = ref({
+        addToCart: () => {
+            console.log("add to cart");
+        },
+        viewPage: (path) => {
+            console.log("View page path :", path)
+        },
+        getProducts: () => {
+            // setSectionState()
+        },
+        searchProducts: () => {
+            // api set state.search.products
+        }
+    });
+
+  return {
+    initialState,
+    state,
+    action,
+    business,
+    sections,
+    setState,
+    setSectionState,
+    setProductState
+  }
+}
