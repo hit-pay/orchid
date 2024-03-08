9740d8602e7cdb708191aaa65f565c761a6463a3
mykhailo <m.diakovych13@gmail.com>
2024-03-08 13:09:34 +0200

    [HIT-6364] Created sandbox banner component

---
+++ packages/vue/src/Elements/SandboxBanner/OcSandboxBanner.stories.js
@@ -0,0 +1,27 @@
+import SandboxBanner from "./OcSandboxBanner.vue";
+
+export default {
+  component: SandboxBanner,
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    title: "You are using test data. Real money won’t be charged",
+    chipProps: {
+      label: "Sandbox Banner",
+      variant: "light-red",
+    },
+  },
+  render: (args) => ({
+    components: {
+      SandboxBanner,
+    },
+    setup() {
+      return { args };
+    },
+    template: `
+          <SandboxBanner :title="args.title" :chipProps="args.chipProps"/>
+        `,
+  }),
+};
---
+++ packages/vue/src/Elements/SandboxBanner/OcSandboxBanner.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Chip } from "@/orchidui";
+
+defineProps({
+  title: {
+    type: String,
+    default: "",
+  },
+  chipProps: {
+    type: Object,
+    default: () => null,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="w-screen relative h-[40px] bg-oc-error flex items-center justify-center text-white"
+  >
+    <span>
+      {{ title || "You are using test data. Real money won’t be charged" }}
+    </span>
+
+    <Chip v-if="chipProps" class="absolute left-[40px]" v-bind="chipProps" />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
