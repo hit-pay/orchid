d28c9d6d3ffad222c0410a5e080c538af8b824ea
kutsaniuk <kutsaniuk@gmail.com>
2024-03-11 21:54:11 +0100

    Fix isSticky

--- packages/vue/src/DataDisplay/Table/OcTable.vue
+++ packages/vue/src/DataDisplay/Table/OcTable.vue
@@ -114,9 +114,9 @@ onMounted(() => onScroll());
 <template>
   <div
     ref="scrollTable"
-    class="grid text-oc-text flex-col border border-oc-gray-200 isolate z-10"
+    class="flex text-oc-text flex-col border border-oc-gray-200 isolate z-10"
     :class="[
-      isSticky ? 'overflow-x-auto' : 'overflow-hidden',
+      isSticky ? 'overflow-x-auto grid' : 'overflow-hidden',
       isResponsive ? 'rounded' : 'md:rounded',
     ]"
     @scroll="onScroll"
