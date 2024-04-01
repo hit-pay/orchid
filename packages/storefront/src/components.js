1dbd74fe2246c3f3c90d3a0a7e490369c1f1c06c
kewcoder <kewcoder@gmail.com>
2024-04-01 09:26:34 +0700

    backup

--- packages/storefront/src/components.js
+++ packages/storefront/src/components.js
@@ -2,66 +2,13 @@
 const components = [
   {
     id: "s-1",
-    n: "Header",
-    g: 'header',
-    c: [
-      {
-        id: "s-2",
-        n: "promotion",
-        css: {
-          all: {
-            s: {
-              'c-4': `
-                display: flex;
-                flex-direction: row;
-                justify-content: center;
-                align-items: center;
-                padding: 11px 56px;
-                gap: 8px;
-                isolation: isolate;
-                width: 100%;
-                height: 48px;
-                background: #111111;
-                flex: none;
-                order: 0;
-                flex-grow: 0;
-                z-index: 6;
-              `
-            }
-          }
-        },
-        code: [
-          {
-            id: "c-4",
-            tp: "text",
-            val: "Banner Promotion"
-          }
-        ]
-      },
-    ],
-    css: {
-      all: {
-        s: {
-          "c-3": "color:blue",
-        },
-        h: {
-          "c-3": "color:red",
-        },
-      },
-      769: {
-        s: {
-          "c-3": "color:yellow",
-        },
-        h: {
-          "c-3": "color:green",
-        },
-      },
-    },
+    name: "Header",
+    group: 'header',
     code: [
       {
-        id: "c-1",
-        tp: "el",
-        val: "s-2"
+        id: "sc-2",
+        tp: "comp",
+        val: "sc-2"
       },
       {
         id: "c-2",
@@ -70,11 +17,92 @@ const components = [
       },
       {
         id: "c-3",
-        tp: "h1",
+        tp: "div",
         val: "Header Title"
+      },
+      {
+        id: "sc-3",
+        tp: "comp",
+        val: "sc-3"
+      }
+    ],
+    css: {
+      all: {
+        s: {
+          "s-1": "height:500px;width:100%",
+          "c-3": "color:blue;height:100px",
+        },
+        h: {
+          "c-3": "color:red",
+        },
+      },
+      769: {
+        s: {
+          "c-3": "color:yellow;",
+        },
+        h: {
+          "c-3": "color:green",
+        },
+      },
+    }
+  },
+  {
+    id: "sc-2",
+    name: "promotion",
+    css: {
+      all: {
+        s: {
+          'c-4': `
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            padding: 11px 56px;
+            gap: 8px;
+            isolation: isolate;
+            width: 100%;
+            height: 48px;
+            background: #111111;
+            flex: none;
+            order: 0;
+            flex-grow: 0;
+            z-index: 6;
+            color:white
+          `
+        }
+      }
+    },
+    code: [
+      {
+        id: "c-4",
+        tp: "text",
+        val: "Banner Promotion"
       }
     ]
-  }
-];
+  },
+  {
+    id: "sc-3",
+    name: "navigation",
+    code: [
+      {
+        id: "c-6",
+        tp: "comp",
+        val: "menu",
+        each: "navigations_menus"
+      }
+    ]
+  },
+  {
+    id: "sc-4",
+    name: "menu",
+    code: [
+      {
+        id: "c-7",
+        tp: "text",
+        val: "Menu"
+      }
+    ]
+  },
+]
 
 export { components as c };
