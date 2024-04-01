075ac87134d40654ac14bef56ec95cd194cf9b0c
kutsaniuk <kutsaniuk@gmail.com>
2024-04-01 15:47:00 +0200

    Add slot and dropdown item

--- packages/vue/src/DataDisplay/FloatContent/OcFloatContent.stories.js
+++ packages/vue/src/DataDisplay/FloatContent/OcFloatContent.stories.js
@@ -18,6 +18,28 @@ export const overview = {
     position: "",
     title: "Transaction detail",
     description: "#9a2804fc-74df-4304-a7d7-79d11f9e1db8",
+    dropdownOptions: {
+      top: [
+        {
+          icon: "pencil",
+          text: "Edit",
+          onClick: () => console.log("edit clicked"),
+        },
+        {
+          icon: "eye-open",
+          text: "View details",
+          onClick: () => console.log("view details clicked"),
+        },
+      ],
+      bottom: [
+        {
+          text: "Delete",
+          icon: "bin",
+          iconClasses: "!text-oc-error",
+          onClick: () => console.log("activate/disactivate clicked"),
+        },
+      ],
+    },
   },
   render: (args) => ({
     components: { FloatContent, Button, Theme },
@@ -33,7 +55,9 @@ export const overview = {
         <FloatContent
             v-model="isOpen"
             :title="args.title"
-            :description="args.description" />
+            :description="args.description" 
+            :dropdown-options="args.dropdownOptions"
+        />
       </Theme>
     `,
   }),
--- packages/vue/src/DataDisplay/FloatContent/OcFloatContent.vue
+++ packages/vue/src/DataDisplay/FloatContent/OcFloatContent.vue
@@ -1,18 +1,38 @@
 <script setup>
-import { Icon } from "@/orchidui";
+import { Dropdown, DropdownItem, Icon } from "@/orchidui";
+import { ref } from "vue";
 
-defineProps({
+const props = defineProps({
   modelValue: {
     type: Boolean,
     default: false,
   },
-  isCloseIcon: {
+  isClose: {
+    type: Boolean,
+    default: true,
+  },
+  isDropdown: {
     type: Boolean,
     default: true,
   },
   title: String,
   description: String,
+  dropdownOptions: {
+    type: Object,
+    default: () => ({
+      top: [],
+      bottom: [],
+    }),
+  },
+  dropdownProps: {
+    type: Object,
+    default: () => ({}),
+  },
 });
+
+const emit = defineEmits(["update:model-value"]);
+
+const isOpen = ref(false);
 </script>
 
 <template>
@@ -23,34 +43,70 @@ defineProps({
       'right-[-999px] w-0': !modelValue,
     }"
   >
-    <div
-      v-if="modelValue"
-      class="flex border-oc-gray-200 gap-x-9 justify-between p-5 items-start border-b"
-    >
-      <slot name="header">
-        <div class="flex flex-col gap-y-1 overflow-hidden">
-          <span
-            class="text-lg font-medium text-ellipsis overflow-hidden whitespace-nowrap"
-          >
-            {{ title }}
-          </span>
-          <slot name="description">
+    <template v-if="modelValue">
+      <div
+        class="flex border-oc-gray-200 gap-x-9 justify-between p-5 items-start border-b"
+      >
+        <slot name="header">
+          <div class="flex flex-col gap-y-1 overflow-hidden">
             <span
-              v-if="description"
-              class="text-sm text-oc-text-300 text-ellipsis overflow-hidden whitespace-nowrap"
+              class="text-lg font-medium text-ellipsis overflow-hidden whitespace-nowrap"
             >
-              {{ description }}
+              <slot name="title">
+                {{ title }}
+              </slot>
             </span>
-          </slot>
-        </div>
-        <div
-          v-if="isCloseIcon"
-          class="p-[6px] rounded cursor-pointer text-oc-gray-500 hover:bg-gray-100 hover:text-oc-text"
-          @click="$emit('update:modelValue', false)"
-        >
-          <Icon name="x" width="20" height="20" />
-        </div>
-      </slot>
-    </div>
+            <slot name="description">
+              <span
+                v-if="description"
+                class="text-sm text-oc-text-300 text-ellipsis overflow-hidden whitespace-nowrap"
+              >
+                {{ description }}
+              </span>
+            </slot>
+          </div>
+          <div class="flex items-center gap-3">
+            <Dropdown
+              v-model="isOpen"
+              :popper-options="{ strategy: 'fixed' }"
+              placement="bottom"
+              class="text-sm"
+              v-bind="dropdownProps"
+            >
+              <div
+                v-if="isDropdown"
+                class="p-[6px] rounded cursor-pointer text-oc-gray-500 hover:bg-gray-100 hover:text-oc-text"
+              >
+                <Icon name="dots-vertical" width="20" height="20" />
+              </div>
+              <template #menu>
+                <div class="p-2 border-b border-gray-200">
+                  <DropdownItem
+                    v-for="(item, i) in dropdownOptions.top"
+                    :key="i"
+                    v-bind="item"
+                  />
+                </div>
+                <div v-if="dropdownOptions.bottom" class="p-2">
+                  <DropdownItem
+                    v-for="(item, i) in dropdownOptions.bottom"
+                    :key="i"
+                    v-bind="item"
+                  />
+                </div>
+              </template>
+            </Dropdown>
+            <div
+              v-if="isClose"
+              class="p-[6px] rounded cursor-pointer text-oc-gray-500 hover:bg-gray-100 hover:text-oc-text"
+              @click="$emit('update:model-value', false)"
+            >
+              <Icon name="x" width="20" height="20" />
+            </div>
+          </div>
+        </slot>
+      </div>
+      <slot name="default"></slot>
+    </template>
   </div>
 </template>
