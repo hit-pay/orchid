9619cab1a14badfd4b5e24ef8245efca34485d6b
HoaiLee <>
2024-04-19 16:08:53 +0700

    Improvement component

--- packages/vue/src/DataDisplay/OnboardingListItem/OcOnboardingListItem.vue
+++ packages/vue/src/DataDisplay/OnboardingListItem/OcOnboardingListItem.vue
@@ -22,6 +22,10 @@ defineProps({
     type: Number,
     default: 1,
   },
+  isButtonVisible: {
+    type: Boolean,
+    default: true,
+  },
 });
 </script>
 
@@ -30,28 +34,37 @@ defineProps({
     <div class="flex-1 flex gap-x-4 items-center">
       <div
         v-if="status === 'completed'"
-        class="w-[24px] flex items-center text-white justify-center h-[24px] rounded-full bg-oc-primary"
+        class="w-[24px] h-[24px] min-w-[24px] flex items-center text-white justify-center rounded-full bg-oc-primary"
       >
         <Icon name="check-2" width="12" height="12" />
       </div>
       <div
         v-else-if="status === 'pending'"
-        class="w-[24px] flex items-center justify-center h-[24px] border rounded-full text-oc-warning border-oc-warning"
+        class="w-[24px] h-[24px] min-w-[24px] flex items-center justify-center border rounded-full text-oc-warning border-oc-warning"
       >
         <Icon name="time-filled" width="18" height="18" />
       </div>
       <div
         v-else
-        class="w-[24px] text-oc-text-300 text-sm font-medium flex items-center justify-center h-[24px] border rounded-full border-oc-gray-200"
+        class="w-[24px] h-[24px] min-w-[24px] text-oc-text-300 text-sm font-medium flex items-center justify-center border rounded-full border-oc-gray-200"
       >
         {{ number }}
       </div>
 
       <div class="flex flex-col gap-y-1">
-        <span class="font-medium">{{ title }}</span>
-        <span class="text-oc-text-300 text-sm">{{ description }}</span>
+        <slot>
+          <span class="font-medium">{{ title }}</span>
+          <span class="text-oc-text-300 text-sm">{{ description }}</span>
+        </slot>
       </div>
     </div>
-    <Button v-bind="buttonOptions" class="shrink-0" />
+
+    <slot name="action">
+      <Button
+        v-if="isButtonVisible"
+        v-bind="buttonOptions"
+        class="shrink-0"
+      />
+    </slot>
   </div>
 </template>
