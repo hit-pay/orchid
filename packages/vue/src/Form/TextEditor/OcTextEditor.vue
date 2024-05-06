354054c9171e06dcba5e645ce98efbfd26dbb6c2
kewcoder <kewcoder@gmail.com>
2024-05-06 16:57:19 +0700

    only accept jpg, jpeg, png, webp and gif

--- packages/vue/src/Form/TextEditor/OcTextEditor.vue
+++ packages/vue/src/Form/TextEditor/OcTextEditor.vue
@@ -176,7 +176,7 @@ const uploadImage = () => {
 
   const input = document.createElement("input");
   input.type = "file";
-  input.accept = "image/*";
+  input.accept = "jpg,.jpeg,.png,.webp,.gif";
   input.onchange = () => {
     const file = input.files[0];
     const reader = new FileReader();
