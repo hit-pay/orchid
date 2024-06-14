caa0cc3faf1fb6cf25081a08d85c74f3f3b166f3
bdebashi <bdebashi@users.noreply.github.com>
2024-06-14 14:53:10 +0100

    store design SubPlanCard component

--- packages/vue/src/StoreDesign.js
+++ packages/vue/src/StoreDesign.js
@@ -3,4 +3,12 @@ import SDMenus from "./StoreDesign/SDMenus/SDMenus.vue";
 import ThumbnailTheme from "./StoreDesign/ThumbnailTheme/ThumbnailTheme.vue";
 import ThumbnailSection from "./StoreDesign/ThumbnailSection/ThumbnailSection.vue";
 import RequestForm from "./StoreDesign/RequestForm/RequestForm.vue";
-export { SDSidebar, SDMenus, ThumbnailTheme, ThumbnailSection, RequestForm };
+import SubPlanCard from "./StoreDesign/SubPlanCard/SubPlanCard.vue";
+export {
+  SDSidebar,
+  SDMenus,
+  ThumbnailTheme,
+  ThumbnailSection,
+  RequestForm,
+  SubPlanCard,
+};
---
+++ packages/vue/src/StoreDesign/SubPlanCard/SubPlanCard.vue
@@ -0,0 +1,158 @@
+<script setup>
+import { computed } from "vue";
+import { Button, Chip, Icon } from "@/orchidui";
+
+const props = defineProps({
+  planTier: {
+    type: Object,
+    required: true,
+  },
+  isPlanActive: Boolean,
+});
+
+const isFreeTier = computed(() => props.planTier.name === "Free");
+const isProTier = computed(() => props.planTier.name === "Pro");
+const isPlusTier = computed(() => props.planTier.name === "Plus");
+
+const frequencyText = computed(
+  () => props.planTier.billing_frequency === "monthly",
+)
+  ? "/month"
+  : "/year";
+
+const planTierDescription = computed(() => {
+  if (props.planTier.name === "free") {
+    return "For individual who just starting their business";
+  } else if (props.planTier.name === "pro") {
+    return "For business who needs more features to increase their sales";
+  } else {
+    return "For business who want in-depth analytics to win business and work";
+  }
+});
+
+const selectPlan = () => {
+  // activate modal for card details
+};
+</script>
+
+<template>
+  <div
+    class="tier-card rounded relative shadow-sm max-w-[300px]"
+    :class="{
+      '!bg-oc-bg-dark border border-oc-gray-200': isFreeTier,
+      'tier-card--pro border border-oc-gray-200': isProTier,
+      'tier-card--plus bg-oc-gray-900 text-white': isPlusTier,
+      'border-2 border-oc-primary-500': isPlanActive,
+    }"
+  >
+    <div class="z-10 flex flex-col gap-4 p-9 relative">
+      <!-- Feature name -->
+      <div class="flex items-center gap-4">
+        <span class="font-medium text-xl">{{ planTier.name }}</span>
+        <Chip v-if="isPlanActive" label="Current Plan" />
+      </div>
+
+      <!-- Feature description -->
+      <div
+        class="text-oc-text-400 text-sm mb-5"
+        :class="{ '!text-oc-text-200': isPlusTier }"
+      >
+        {{ planTierDescription }}
+      </div>
+
+      <!-- Feature price -->
+      <div class="flex gap-3 items-center mb-5">
+        <div class="flex items-start gap-2">
+          <span class="text-[24px] font-medium tracking-tighter">{{
+            planTier.currency.toUpperCase()
+          }}</span>
+          <span class="text-[40px] leading-none font-medium">{{
+            planTier.price
+          }}</span>
+        </div>
+        <span
+          v-if="!isFreeTier"
+          class="text-oc-text-400 text-base"
+          :class="{ '!text-oc-text-200': isPlusTier }"
+        >
+          {{ frequencyText }}
+        </span>
+      </div>
+
+      <!-- Button -->
+      <Button
+        v-if="!isFreeTier"
+        label="Start 14 day trial"
+        size="big"
+        class="mb-5"
+        @click="selectPlan"
+      />
+
+      <!-- Features -->
+      <div
+        class="flex flex-col gap-3"
+        :class="{ 'text-oc-text-200': isPlusTier }"
+      >
+        <span v-if="!isFreeTier">
+          Everything on
+          <strong>{{ planTier.code === "tier_s" ? "FREE" : "PRO" }}</strong>
+          includes:
+        </span>
+
+        <span
+          v-for="feature in planTier.features"
+          :key="feature.id"
+          class="flex gap-3 items-center"
+        >
+          <Icon
+            name="check-fill"
+            class="text-oc-primary-500"
+            height="16"
+            width="16"
+          />
+          <span>{{ feature.name }}</span>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.tier-card {
+  &--pro {
+    &::after {
+      position: absolute;
+      height: 50%;
+      right: 0;
+      top: 0;
+      left: 0;
+      content: "";
+      border-radius: 16px;
+      background: linear-gradient(
+        90deg,
+        rgba(232, 201, 250, 0.3) 0%,
+        rgba(255, 224, 162, 0.3) 100%
+      );
+      box-shadow: 0 0 60px 30px #ffffff inset;
+    }
+  }
+
+  &--plus {
+    &::after {
+      position: absolute;
+      height: 25%;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      content: "";
+      border-radius: 16px;
+      background: linear-gradient(
+        90deg,
+        rgba(232, 201, 250, 0.3) 0%,
+        rgba(255, 224, 162, 0.3) 100%
+      );
+      box-shadow: 0 0 60px 30px var(--oc-gray-900) inset;
+    }
+  }
+}
+</style>
