db0bd359f0faf69c48889f579a9ef60f853f2259
Hoai Le <40382842+HoaiLee@users.noreply.github.com>
2024-07-01 14:05:32 +0700

    [Chore] Modal page slot (#706)
    * Add slot
    
    * v0.5.380
    
    ---------
    
    Co-authored-by: HoaiLee <>

--- packages/vue/src/Overlay/Modal/OcModalPage.stories.js
+++ packages/vue/src/Overlay/Modal/OcModalPage.stories.js
@@ -1,4 +1,4 @@
-import { Theme, Button, OcModalPage } from '@/orchidui'
+import { Theme, Button, OcModalPage, Icon } from '@/orchidui'
 import { ref } from 'vue'
 
 export default {
@@ -9,7 +9,7 @@ export const Default = {
   argTypes: {},
   args: {},
   render: (args) => ({
-    components: { Theme, Button, OcModalPage },
+    components: { Theme, Button, OcModalPage, Icon },
     setup() {
       const modalValue = ref(false)
       return { modalValue, args }
@@ -24,6 +24,18 @@ export const Default = {
             is-close-icon-visible
             is-back-button-visible
           >
+            <template #top>
+              <div class="flex relative">
+                <div>
+                  <Icon  name="hitPay" width="24" height="24" />
+                </div>
+
+                <div class="ml-auto relative">
+                  <Icon name="pencil" width="18" height="18" />
+                </div>
+              </div>
+            </template>
+
             <div class="max-w-[980px] mx-auto">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde nesciunt vitae, quis maxime explicabo numquam molestias natus dolores quidem. Provident eligendi rem perferendis tempora ex magnam officia fugiat quae? Ex!
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde nesciunt vitae, quis maxime explicabo numquam molestias natus dolores quidem. Provident eligendi rem perferendis tempora ex magnam officia fugiat quae? Ex!
--- packages/vue/src/Overlay/Modal/OcModalPage.vue
+++ packages/vue/src/Overlay/Modal/OcModalPage.vue
@@ -53,27 +53,31 @@ const onClickOutside = async () => {
   >
     <div class="w-full h-full absolute top-0 left-0 z-[1007]" @click="onClickOutside" />
 
-    <div
-      class="relative z-[1008] shadow-normal w-[calc(100%-40px)] h-[calc(100%-40px)] max-w-[1440px] bg-[linear-gradient(180deg,_rgba(229,_238,_255,_0.5)_0%,_rgba(229,_238,_255,_0)_77.75%)] rounded-xl flex flex-col overflow-y-auto"
-    >
-      <div
-        v-if="isBackButtonVisible"
-        class="py-3 px-4 rounded cursor-pointer text-oc-gray-400 hover:bg-gray-100 hover:text-oc-text absolute top-5 left-5"
-        @click="emit('back')"
-      >
-        <Button variant="secondary" is-transparent label="Back" left-icon="chevron-left" />
-      </div>
+    <div class="w-[calc(100%-40px)] h-[calc(100%-40px)] z-[1008]">
+      <slot name="top" />
 
       <div
-        class="flex gap-3 py-3 px-4 rounded cursor-pointer text-oc-gray-400 hover:bg-gray-100 hover:text-oc-text absolute top-5 right-5"
-        @click="emit('update:model-value', false)"
+        class="relative z-[1008] shadow-normal max-w-[1440px] bg-[linear-gradient(180deg,_rgba(229,_238,_255,_0.5)_0%,_rgba(229,_238,_255,_0)_77.75%)] rounded-xl flex flex-col overflow-y-auto"
       >
-        <Icon name="x" width="18" height="18" />
-        <span>Close</span>
-      </div>
+        <div
+          v-if="isBackButtonVisible"
+          class="py-3 px-4 rounded cursor-pointer text-oc-gray-400 hover:bg-gray-100 hover:text-oc-text absolute top-5 left-5"
+          @click="emit('back')"
+        >
+          <Button variant="secondary" is-transparent label="Back" left-icon="chevron-left" />
+        </div>
 
-      <div class="modal-body p-9">
-        <slot />
+        <div
+          class="flex gap-3 py-3 px-4 rounded cursor-pointer text-oc-gray-400 hover:bg-gray-100 hover:text-oc-text absolute top-5 right-5"
+          @click="emit('update:model-value', false)"
+        >
+          <Icon name="x" width="18" height="18" />
+          <span>Close</span>
+        </div>
+
+        <div class="modal-body p-9">
+          <slot />
+        </div>
       </div>
     </div>
   </div>
