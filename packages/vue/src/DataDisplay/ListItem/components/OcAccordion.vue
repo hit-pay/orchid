b871c5b1d7dfff1b3823fa104c8bf149c9303018
HoaiLee <lequanghoai28@gmail.com>
2024-10-21 10:32:54 +0700

    Add prop: hasActions for list item

--- packages/vue/src/DataDisplay/ListItem/components/OcAccordion.vue
+++ packages/vue/src/DataDisplay/ListItem/components/OcAccordion.vue
@@ -10,6 +10,10 @@ defineProps({
     default: () => []
   },
   isDisabled: Boolean,
+  hasActions: {
+    type: Boolean,
+    default: true,
+  },
   isTransparent: Boolean,
   isDraggable: Boolean
 })
@@ -73,7 +77,7 @@ const toggleAccordion = () => {
         </div>
 
         <slot name="right">
-          <div class="flex items-center gap-4 opacity-0 group-hover:opacity-100">
+          <div v-if="hasActions" class="flex items-center gap-4 opacity-0 group-hover:opacity-100">
             <div class="border border-oc-accent-1-100 rounded-sm p-1 flex gap-x-1">
               <Icon
                 name="pencil"
