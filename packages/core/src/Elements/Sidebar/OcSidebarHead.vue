937dab1cdceddab8a77c9be10ae23c1d043ff553
mykhailo <m.diakovych13@gmail.com>
2025-01-09 10:28:13 +0200

    Fix updated sidebar UI

--- packages/core/src/Elements/Sidebar/OcSidebarHead.vue
+++ packages/core/src/Elements/Sidebar/OcSidebarHead.vue
@@ -1,25 +1,16 @@
 <template>
   <div>
-    <div class="pb-2" @click="isExpanded = !isExpanded">
+    <div class="pb-2">
       <div
         v-if="isSidebarExpanded && label"
-        class="pl-3 uppercase relative text-medium text-sm" 
-        :class="isExpanded ? 'text-oc-accent-1-200' : 'text-oc-accent-1-300'"
+        class="pl-3 uppercase text-oc-accent-1-200 relative text-medium text-sm" 
       >
-        <Icon 
-          name="sidebar-header" 
-          width="18" 
-          height="18" 
-          class="absolute -left-5 top-0 transition-all duration-300 ease-in-out w-[18px] h-[18px]" 
-          :class="isExpanded ? '' : '-rotate-90'" 
-        />
         <span>{{ label }}</span>
       </div>
     </div>
     <div
       class="grid transition-all grid-rows-[1fr] duration-300 ease-in-out"
       :class="{
-        '!grid-rows-[0fr]': !isExpanded && isSidebarExpanded,
         'overflow-hidden': isSidebarExpanded,
       }"
     >
@@ -31,13 +22,8 @@
 </template>
 
 <script setup>
-import { Icon } from '@/orchidui-core'
-import { ref } from 'vue'
-
 defineProps({
   label: String,
   isSidebarExpanded: Boolean
 })
-
-const isExpanded = ref(true)
 </script>
