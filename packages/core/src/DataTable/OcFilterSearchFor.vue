0a5b49f3aa13734b50d81c9e3273bd9da3313021
HoaiLee <lequanghoai28@gmail.com>
2025-09-01 14:54:33 +0700

    HIT-13775

--- packages/core/src/DataTable/OcFilterSearchFor.vue
+++ packages/core/src/DataTable/OcFilterSearchFor.vue
@@ -21,11 +21,11 @@ const emit = defineEmits({
 
 const filterData = computed(() => {
   const filterData = []
-  
+
   props.filters?.forEach((filter) => {
     if (filter.multiNames) {
       const exist = filterData.find((f) => f.name === filter.multiNames[0])
-      
+
       if (!exist) {
         filterData.push(filter)
       } else {
@@ -35,13 +35,13 @@ const filterData = computed(() => {
       filterData.push(filter)
     }
   })
-  
+
   return filterData
 })
 
 const removeFilter = (name, multiNames) => {
   const filter = { [name]: '' }
-  
+
   if (multiNames) {
     multiNames.forEach((filterName) => {
       const multifilter = { [filterName]: '' }
@@ -60,30 +60,32 @@ const removeFilter = (name, multiNames) => {
   >
     <div class="flex gap-1 items-center normal-case flex-wrap">
       <span class="pr-2 text-sm font-medium text-oc-text">Search for:</span>
-      
+
       <Chip
         v-for="query in queries"
         :key="query"
         variant="accent-1"
         closable
         :label="query"
+        class="text-wrap"
         @remove="$emit('removeQuery', query)"
       />
-      
+
       <Chip
         v-for="item in filterData"
         :key="item.name"
         variant="accent-1"
         closable
         :label="item.label"
+        class="text-wrap"
         @remove="removeFilter(item.name, item.multiNames)"
       />
-      
-      <Chip 
-        variant="gray" 
-        class="cursor-pointer" 
-        label="Clear all" 
-        @click="$emit('removeAll')" 
+
+      <Chip
+        variant="gray"
+        class="cursor-pointer"
+        label="Clear all"
+        @click="$emit('removeAll')"
       />
     </div>
   </TableHeader>
