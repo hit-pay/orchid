8bb8ac49b81bbab79d447b5a07dbf042d2c447b0
Hoai Le <40382842+HoaiLee@users.noreply.github.com>
2025-10-22 13:59:48 +0700

    [Improvement] Update topbanner component (#1193)
    * Update topbanner component
    
    * Text content center

--- packages/core/src/Elements/TopBanner/OcTopBanner.vue
+++ packages/core/src/Elements/TopBanner/OcTopBanner.vue
@@ -1,33 +1,58 @@
 <script setup>
-import { Button, Icon } from '@/orchidui-core'
+import { Button, Icon, Chip } from '@/orchidui-core'
 
 defineProps({
+  chipProps: {
+    type: Object,
+    required: false,
+    default: null
+  },
   title: {
     type: String,
     default: ''
   },
-  buttonLabel: String
+  rawHtml: {
+    type: String,
+    default: null
+  },
+  buttonLabel: String,
+  titleClass: String
 })
 
 defineEmits(['click:button'])
 </script>
 
 <template>
-  <div class="w-screen relative h-[40px] bg-oc-warning-300 flex items-center justify-center">
-    <span class="text-sm font-medium">
-      {{ title }}
-    </span>
+  <div class="w-screen relative bg-oc-warning-300 flex items-center justify-center px-5">
+    <Chip
+      v-if="chipProps"
+      v-bind="chipProps"
+    />
 
-    <Button
-      v-if="buttonLabel"
-      class="absolute right-9"
-      size="small"
-      is-transparent
-      @click="$emit('click:button')"
-    >
-      <span class="flex items-center gap-2 underline">
-        {{ buttonLabel }} <Icon name="arrow-right" width="9" height="9" />
-      </span>
-    </Button>
+    <div class="flex-1 flex flex-col items-center md:flex-row gap-3 md:gap-5">
+      <div v-if="title" :class="['flex-1 mx-auto flex justify-center', titleClass]">
+        <span class="text-sm font-medium">
+          {{ title }}
+        </span>
+      </div>
+
+      <div
+        v-else-if="rawHtml"
+        :class="['text-sm font-medium flex-1 flex justify-center', titleClass]"
+        v-html="rawHtml"
+      />
+
+      <Button
+        v-if="buttonLabel"
+        class="md:ml-auto mr-auto"
+        size="small"
+        is-transparent
+        @click="$emit('click:button')"
+      >
+        <span class="flex items-center gap-2 underline">
+          {{ buttonLabel }} <Icon name="arrow-right" width="9" height="9" />
+        </span>
+      </Button>
+    </div>
   </div>
 </template>
