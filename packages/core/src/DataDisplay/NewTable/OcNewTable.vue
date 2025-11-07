7ca6a2eff1cfe4ab8e9346970a5b719d70174dd4
mykhailo <m.diakovych13@gmail.com>
2025-11-07 12:46:02 +0200

    fix: remove unnecessary comma in class binding for table header

--- packages/core/src/DataDisplay/NewTable/OcNewTable.vue
+++ packages/core/src/DataDisplay/NewTable/OcNewTable.vue
@@ -50,7 +50,7 @@
               >
                 <div
                   class="px-5 py-3 truncate min-h-[34px] font-medium text-xs border-b border-oc-text-200 uppercase flex items-center gap-2 justify-between w-full"
-                  :class="{ 'h-[inherit]': header.key === 'actions',  }"
+                  :class="{ 'h-[inherit]': header.key === 'actions'  }"
                 >
                 <Tooltip v-if="header.leftTooltip" :popper-options="{ strategy: 'fixed' }">
                     <Icon :name="header.leftIcon" width="16" height="16" class="text-oc-text-400" v-bind="header.leftIconProps" />
