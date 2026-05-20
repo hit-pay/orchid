98cb7b0bd9543eba60c0c44b821892b21813d9af
Ari Bahtiar <kewcoder@gmail.com>
2026-05-20 09:28:36 +0700

    fix

--- packages/dashboard/src/Dashboard/Charts/OverviewPieChart/OcOverviewPieChart.stories.js
+++ packages/dashboard/src/Dashboard/Charts/OverviewPieChart/OcOverviewPieChart.stories.js
@@ -14,6 +14,25 @@ export const Default = {
       { value: 200, name: 'Paynow', itemStyle: { color: '#AEC5FF' } },
       { value: 100, name: 'Others', itemStyle: { color: '#86FFCC' } }
     ],
+    tooltip: {
+      show: true,
+      trigger: 'item',
+      padding: 0,
+      formatter: (params) => `
+        <div class="py-3 px-4 leading-normal">
+          <div class="uppercase text-[10px] font-medium text-oc-text-400">
+            ${params.name}
+          </div>
+          <div class="text-oc-text-500 font-medium text-base">
+            SGD ${Number(params.value).toLocaleString('en-US', {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2
+            })}
+            (${params.percent}%)
+          </div>
+        </div>
+      `
+    },
     showPercentInLabel: true,
     totalLabel: 'Total Customer'
   },
