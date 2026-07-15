9dabfcf9a0c24d2e590f49ff2675802b84bf5393
Ari Bahtiar <kewcoder@gmail.com>
2026-07-15 17:24:46 +0700

    add custom popperStyle to Select component

---
+++ packages/core/src/Form/Select/examples/CustomWidth.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+import { Select } from '@/orchidui-core'
+
+const value = ref(null)
+
+const options = [
+  { label: 'Republic of Indonesia', value: 'ID' },
+  { label: 'Republic of Singapore', value: 'SG' },
+  { label: 'Kingdom of Thailand', value: 'TH' },
+  { label: 'Socialist Republic of Vietnam', value: 'VN' }
+]
+</script>
+
+<template>
+  <div class="w-[220px] h-[320px]">
+    <Select
+      v-model="value"
+      label="Country"
+      :popper-style="{ width: '500px' }"
+      placeholder="Select..."
+      is-filterable
+      :options="options"
+    />
+  </div>
+</template>
---
+++ packages/core/src/Form/Select/examples/MultiSelect.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import { Select } from '@orchidui/core'
+
+const values = ref([])
+
+const options = [
+  { label: 'Design',      value: 'design' },
+  { label: 'Engineering', value: 'eng' },
+  { label: 'Marketing',   value: 'mkt' },
+  { label: 'Sales',       value: 'sales' },
+  { label: 'Support',     value: 'support' }
+]
+</script>
+
+<template>
+  <div class="w-[350px] h-[300px]">
+    <Select
+      v-model="values"
+      label="Departments"
+      placeholder="Select departments..."
+      multiple
+      is-filterable
+      is-checkboxes
+      is-select-all
+      :options="options"
+    />
+  </div>
+</template>
